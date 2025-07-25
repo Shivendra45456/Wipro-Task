697f2949ad7e8bfdcf54eb0f8ab47041b80195a8
Shivendra Patel <95277417+Shivendra45456@users.noreply.github.com>
2025-07-25 16:22:55 +0530

    Add files via upload

---
+++ TaskFinalKeyword/Sample1.java
@@ -0,0 +1,19 @@
+package TaskFinalKeyword;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final double PI = 3.14;
+		//PI=5;
+		System.out.println(PI);
+
+	}
+
+}
+
+//Final Keyword
+//Task 1: Final Variables
+// Declare a final variable PI = 3.14.
+// Try to modify its value (observe compile error).
